ce9dba6a3b9c9272dfce32cca8b4a0c68cee5980
hedanta <95635887+hedanta@users.noreply.github.com>
2022-10-18 22:32:05 +0300

    Update 1512b.cpp

--- 1512b.cpp
+++ 1512b.cpp
@@ -1,4 +1,4 @@
-//https://codeforces.com/problemset/problem/1512/A
+//https://codeforces.com/problemset/problem/1512/B
 //BPM-22-2 Lebkova Marina
 
 #include <iostream>
@@ -8,47 +8,96 @@
 #include <cmath>
 
 int main() {
-    int test = 0;
-    std::cin >> test;
+  int test_num = 0;
+  std::cin >> test_num;
 
-    for (int iTest = 0; iTest < test; iTest += 1) {
-        int n = 0;
-        std::cin >> n;
-        std::vector<std::vector<char>> table;
+  for (int iTest = 0; iTest < test_num; iTest += 1) {
+    int table_size = 0;
+    std::vector<std::vector<char>> table;
+    
+    std::cin >> table_size;
 
-        int x1 = 0, y1 = 0;
-        int x2 = 0, y2 = 0;
-        bool first = 0;
+    //координаты двух углов
+    int x1 = 0;
+    int y1 = 0;
+    int x2 = 0;
+    int y2 = 0;
+    
+    bool first_cell = true; //клетка с '*' встретилась впервые
 
-        for (int iRow = 0; iRow < n; iRow += 1) {
-            for (int iCol = 0; iCol < n; iCol += 1) {
-                char c;
-                std::cin >> c;
-                table[iRow].push_back(c);
-                std::cin >> table[iRow][iCol];
-                if (c == '*') {
-                    if (first) {
-                        x2 = iRow;
-                        y2 = iCol;
-                    }
-                    else {
-                        x1 = iRow;
-                        y1 = iCol;
-                    }
-                }
-            }
+    for (int iRow = 0; iRow < table_size; iRow += 1) {
+      std::vector<char> cur_row;
+      for (int iCol = 0; iCol < table_size; iCol += 1) {
+        char element = ' ';
+
+        std::cin >> element;
+        cur_row.push_back(element);
+
+        if (element == '*') {
+          if (first_cell) {
+            x1 = iRow;
+            y1 = iCol;
+            first_cell = false;
+          }
+
+          else {
+            x2 = iRow;
+            y2 = iCol;
+          }
         }
 
-        table[x2][y1] = '*';
-        table[x1][y2] = '*';
+      }
 
-        for (int iRow = 0; iRow < n; iRow += 1) {
-            for (int iCol = 0; iCol < n; iCol += 1) {
-                std::cout << table[iRow][iCol];
-            }
-            std::cout << '\n';
-        }
-
+      table.push_back(cur_row);
     }
-    return 0;
-} 
+
+    //'*' в одной строке
+    if (x1 == x2) {
+      //положение двух других клеток в противоположной строке
+      int ind_x = 0; //строка с двумя другими клетками
+
+      if (x1 != 0 && x1 != table_size - 1) {
+        ind_x = x1 + 1;
+      }
+
+      else {
+        ind_x = table_size - 1 - x1;
+      }
+
+      table[ind_x][y1] = '*';
+      table[ind_x][y2] = '*';
+    }
+
+    //'*' в одном столбце
+    else if (y1 == y2) {
+      //положение двух других клеток в противоположном столбце
+      int ind_y = 0; //столбец с двумя другими клетками
+
+      if (y1 != 0 && y1 != table_size - 1) {
+        ind_y = y1 + 1;
+      }
+
+      else {
+        ind_y = table_size - 1 - y1;
+      }
+
+      table[x1][ind_y] = '*';
+      table[x2][ind_y] = '*';
+    }
+
+    //'*' в двух разных углах
+    else {
+      //клетки противоположны имеющимся
+      table[x1][y2] = '*';
+      table[x2][y1] = '*';
+    }
+
+    for (int iRow = 0; iRow < table_size; iRow += 1) {
+      for (int iCol = 0; iCol < table_size; iCol += 1) {
+        std::cout << table[iRow][iCol];
+      }
+      std::cout << '\n';
+    }
+  }
+  return 0;
+}
