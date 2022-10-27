d1a1177a79b1fde7d83c2e2b296a662180b3076a
hedanta <95635887+hedanta@users.noreply.github.com>
2022-10-27 13:52:56 +0300

    Create 1699b.cpp

---
+++ 1699b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int main() {
+  int n;
+  std::cin >> n;
+  int ans = 0;
+  while (n > 0) {
+    if (n & 1) {
+	    ans += 1;
+    }
+    n = n >> 1;
+  }
+  std::cout << ans;
+  return 0;
+} 
