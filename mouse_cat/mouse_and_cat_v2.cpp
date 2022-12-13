ee0ddd8e59f5e5e907b7d1035640946e035baaab
hedanta <95635887+hedanta@users.noreply.github.com>
2022-12-13 10:55:50 +0300

    v = const

--- mouse_cat/mouse_and_cat_v2.cpp
+++ mouse_cat/mouse_and_cat_v2.cpp
@@ -9,73 +9,75 @@
 const double EPS = std::numeric_limits<double>::epsilon();
 
 int main() {
-	// положение виновников задачи
-	Rdec2D r_mouse = { 0.0, -30.0 };
-	Rdec2D r_cat = { 30.0, 0.0 };
+  // положение виновников задачи
+  Rdec2D r_mouse = { 0.0, -3.0 };
+  Rdec2D r_cat = { 3.0, 0.0 };
 
-	// зона поржения
-	double kill_dist = 0.5;
+  // зона поржения
+  double kill_dist = 0.5;
 
-	// коэффициент для ускорения мышки
-	double k = 0.001;
+  // коэффициент для ускорения мышки
+  double k = 0.001;
 
-	double dt = 0.05;
+  double dt = 0.1;
 
-	// начальные скорости
-	Rdec2D v0_mouse = { 0.0, 2.5 };
-	Rdec2D v0_cat = { 2.5, 0.05 };
-	Rdec2D u_mouse = v0_mouse;
-	Rdec2D u_cat = v0_cat;
+  // начальные скорости
+  double mod_v0_cat = 1.0;
+  double mod_v0_mouse = 1.0;
+  Rdec2D u_cat = (r_mouse - r_cat) / Norm(r_mouse - r_cat) * mod_v0_cat;
+  Rdec2D u_mouse = r_mouse / Norm(r_mouse) * mod_v0_mouse;
 
-	// предыдущее расстояние между героями событий
-	double prev = Norm(r_mouse - r_cat);
+  // предыдущее расстояние между героями событий
+  double prev = Norm(r_mouse - r_cat);
 
-	std::ofstream out("C://work//mouse_cat//result.txt");
+  std::ofstream out("C://work//mouse_cat//result.txt");
 
-	int i = 1;
+  int i = 1;
 
-	// реализация
-	while (true) {
-		if (out.is_open()) {
-			out << std::fixed << std::setprecision(3) << i << " " << r_cat.x << " " << r_cat.y << " " << r_mouse.x << " " << r_mouse.y << " " << Norm(r_mouse - r_cat) << '\n';
-		}
+  // реализация
+  while (true) {
+    if (out.is_open()) {
+      out << std::fixed << std::setprecision(3) << i << " " << r_cat.x << " " << r_cat.y << " " << r_mouse.x << " " << r_mouse.y << " " << Norm(r_mouse - r_cat) << '\n';
+    }
 
-		// обновление положения кошки
-		r_cat = r_cat - u_cat * dt;
+    // обновление скоростей 
+    u_cat = (r_mouse - r_cat) / Norm(r_mouse - r_cat) * mod_v0_cat;
+    u_mouse = r_mouse / Norm(r_mouse) * mod_v0_mouse;
 
-		// обновление положения мышки
-		r_mouse = r_mouse + u_mouse * dt;
+    // обновление положений
+    r_cat = r_cat + u_cat * dt;
+    r_mouse = r_mouse - u_mouse * dt;
 
-		// мыш дырыш
-		// мышка "выше" точки (0, 0) - убежала в норку
+    // мыш дырыш
+    // мышка "выше" точки (0, 0) - убежала в норку
+    if (r_mouse.y >= 0) {
+      i += 1;
+      out << std::fixed << std::setprecision(3) << i << " " << r_cat.x << " " << r_cat.y << " " << r_mouse.x << " " << r_mouse.y << " " << Norm(r_mouse - r_cat) << '\n';
+      std::cout << "mouse is in hole";
+      break;
+    }
 
-		if (r_mouse.y == 0 && r_cat.x == 0) {
-			std::cout << "hooray\n";
-			break;
-		}
+    // мыш кирдыш
+    // кошка поймала мышку
+    if (Norm(r_mouse - r_cat) <= kill_dist) {
+      i += 1;
+      out << std::fixed << std::setprecision(3) << i << " " << r_cat.x << " " << r_cat.y << " " << r_mouse.x << " " << r_mouse.y << " " << Norm(r_mouse - r_cat) << '\n';
+      std::cout << "rip mouse";
+      break;
+    }
 
-		if (r_mouse.y >= 0) {
-			std::cout << "mouse is in hole";
-			break;
-		}
+    // мыш победыш
+    // расстояние между кошкой и мышкой увеличивается 
+    // или кошка "перебежала" норку
+    if (r_cat.x < 0 || Norm(r_mouse - r_cat) > prev) {
+      i += 1;
+      out << std::fixed << std::setprecision(3) << i << " " << r_cat.x << " " << r_cat.y << " " << r_mouse.x << " " << r_mouse.y << " " << Norm(r_mouse - r_cat) << '\n';
+      std::cout << "mouse will be in hole";
+      break;
+    }
 
-		// мыш кирдыш
-		// кошка поймала мышку
-		if (Norm(r_mouse - r_cat) <= kill_dist) {
-			std::cout << "rip mouse";
-			break;
-		}
-
-		// мыш победыш
-		// расстояние между кошкой и мышкой увеличивается 
-		// или кошка "перебежала" норку
-		if (r_cat.x < 0 || Norm(r_mouse - r_cat) > prev) {
-			std::cout << "mouse will be in hole";
-			break;
-		}
-
-		prev = Norm(r_mouse - r_cat);
-		i += 1;
-	}
-	out.close();
+    prev = Norm(r_mouse - r_cat);
+    i += 1;
+  }
+  out.close();
 }
