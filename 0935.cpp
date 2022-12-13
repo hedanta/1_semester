31281eb0698a6156b6ef4608c4082342b4c989f4
hedanta <95635887+hedanta@users.noreply.github.com>
2022-12-13 13:56:00 +0300

    Update 0935.cpp

--- 0935.cpp
+++ 0935.cpp
@@ -6,33 +6,26 @@
 #include <array>
 
 int main() {
-    int n = 0;
-    std::cin >> n;
-
-    std::string way = "";
-    std::cin >> way;
-
-    int curx = 0;
-    int cury = 0;
-
-
-    int money = 0;
-
-    for (int iSt = 0; iSt < way.size() - 1; iSt += 1) {
-        if (way[iSt] == 'U') {
-            curx += 1;
-        }
-
-        else {
-            cury += 1;
-        }
-        if (curx - cury == 0) {
-            if (way[iSt] == way[iSt + 1]) {
-                money += 1;
-            }
-        }
+  int n = 0;
+  std::cin >> n;
+  std::string way = "";
+  std::cin >> way;
+  int curx = 0;
+  int cury = 0;
+  int money = 0;
+  for (int iSt = 0; iSt < way.size() - 1; iSt += 1) {
+    if (way[iSt] == 'U') {
+      curx += 1;
     }
-
-    std::cout << money;
-    return 0;
+    else {
+      cury += 1;
+    }
+    if (curx - cury == 0) {
+      if (way[iSt] == way[iSt + 1]) {
+        money += 1;
+      }
+    }
+  }
+  std::cout << money;
+  return 0;
 } 
